34e23ea16df981835e8a54a53de4b3d89fbabd29
Tijam Moradi <tijam.moradi@cgi.com>
2021-06-07 11:38:35 +0300

    Initial Commit

---
+++ FoodNav/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FoodNav
+//
+//  Created by Tijam Moradi on 7.6.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
